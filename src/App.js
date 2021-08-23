1a534c2e652bf52ed58dd4380f215cbc345133ae
Simon Burg <semburg.sb@gmail.com>
2021-08-23 11:17:59 +0200

    stylings start

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React, { useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/Counter';
+import './styles/App.css'
 
 // 22-04   23/8
 
 
 function App() {
 
-  const [likes, setLikes] = useState(0)
   const [value, setValue] = useState('Input Text')
 
 
 
   return (
     <div className="App">
-      <h3>Examples</h3>
-      <Counter/>
-      <ClassCounter/>
+      
+      <div className="post">
+          <div className="post__content">
+            <strong>1. Javascript</strong>
+            <div>
+              Javascript - programming language
+            </div>
+          </div>
+          <div className="post__btns">
+            <button>Delete</button>
+          </div>
+      </div>
+
     </div>
   );
 }
