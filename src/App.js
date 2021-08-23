1dcfefc68024dbd3229ed82b89a3a56b9247a3a0
Simon Burg <semburg.sb@gmail.com>
2021-08-23 10:46:23 +0200

    components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-
+// 22-04   23/8
 
 
 function App() {
-  
+
   const [likes, setLikes] = useState(0)
+  const [value, setValue] = useState('Input Text')
 
   function increment() {
-   setLikes(likes+1)
+    setLikes(likes + 1)
   }
 
   function decrement() {
-    setLikes(likes-1)
+    setLikes(likes - 1)
   }
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>{value}</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={event => setValue(event.target.value)} />
       <h2>{likes}</h2>
       <button onClick={increment} >Increment</button>
       <button onClick={decrement}>Decrement</button>
